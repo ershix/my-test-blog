df38e098a7a6d4d4d922d0495aaaa582d7563a85
ershix <ershix@mail.ru>
2017-06-06 14:01:30 +0300

    Added host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*bpqy(txsmlx2)$zkp(c&vgzsk=6l762crztki5jm-c6r+b8f6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ershix.pythonanywhere.com']
 
 
 # Application definition
